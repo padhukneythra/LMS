51340141e7dd3c6b2d0ae2ff4e4592ba812f3788
padhukneythra <padmavathy20may@gmail.com>
2025-03-19 15:08:11 +0530

    Updated style.css and removed script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function openModal(title, details) {
-    document.getElementById("modalTitle").innerText = title;
-    document.getElementById("modalBody").innerHTML = details;
-    var modal = new bootstrap.Modal(document.getElementById("courseModal"));
-    modal.show();
-}
